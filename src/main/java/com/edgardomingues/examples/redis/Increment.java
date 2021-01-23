6718908723f8bd4e3135b4a30ca0b3957404799c
Edgar Domingues <edgar.domingues@talkdesk.com>
2021-01-23 19:46:03 +0000

    Quarkus Redis client example

---
+++ src/main/java/com/edgardomingues/examples/redis/Increment.java
@@ -0,0 +1,14 @@
+package com.edgardomingues.examples.redis;
+
+public class Increment {
+    public String key;
+    public int value;
+
+    public Increment(String key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public Increment() {
+    }
+}
